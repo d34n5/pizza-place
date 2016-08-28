98c6cb9eb5fab49dd58e2750116bd030113d2db3
Dean Scelza <deanscelza@gmail.com>
2016-08-27 20:13:19 -0700

    now return ALL toppings, not just 1st selected

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,44 +1,37 @@
 function Pizza (size, toppings) {
   this.size = size;
-  this.toppings = toppings;
+  this.toppings = [];
 }
 
-//Pizza.prototype.pizzaSale = function() {
-  // return this.sized + "-sized Pizza, topped with: " + this.toppings ;
-//}
+Pizza.prototype.pizzaSale = function() {
+  return this.size + "-sized Pizza, topped with: " + this.toppings ;
+}
 
- 
-
-
-// function resetFields() {
-//   $(".sizes").val("");
-//   $("input.pizzaToppings").val("");
-// }
-
-
+//---------logic split -----------//
 
 $(document).ready(function() {
   $("form#orderPizza").submit(function(event) {
     event.preventDefault();
 
-    // var sizeOrdered = $(".sizes").??
-    // var toppingsOrdered = $(".toppings").??
+    var sizeOrdered = $("#selectId option:selected").attr("id")
+    var toppingsOrdered = $(":checkbox").map(function() {
+      return this.id;
+    })
+    .get()
+    .join();
 
     var sizeCost = parseInt($(".sizes").val());
     var toppingsCost = parseInt($("input:checkbox:checked").val());
 
-    var pizzaCost = (sizeCost + toppingsCost + "$");
-    var pizzaOrdered = new Pizza (Pizza.sized, Pizza.toppings);
+    var pizzaCost = (sizeCost + toppingsCost + "$" );
+    var pizzaOrdered = (sizeOrdered + "-sized Pizza - Topped with: " + toppingsOrdered)
 
     $("#costDisplay").show();
     $("#costDisplay").text(pizzaCost);
 
-    // $("#orderDetails").show();
-    // $("#orderDetails").text(pizzaOrdered);
+    $("#orderDetails").show();
+    $("#orderDetails").text(pizzaOrdered);
 
-    // $("#orderDetails").text(newPizza.pizzaSale());
-
-
-
+    $("#orderDetails").text(newPizza.pizzaSale());
   });
 });
