8b871caed4544e3d53754924f85a9ffcd478c8e2
Dean Scelza <deanscelza@gmail.com>
2016-08-29 19:16:29 -0700

    clean up logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,29 @@
-function Pizza (size, toppingList, toppingCount) {
+function Pizza (size, toppingList) {
   this.size = size;
   this.toppingList = toppingList;
-  this.toppingCost = toppingCost;
   this.price = 0;
 }
 
-Pizza.prototype.pizzaCost = function(size, toppings) {
-  this.price += (size + (toppings*2));
+Pizza.prototype.pizzaCost = function() {
+  this.price = (this.size + (this.toppingList.length*2));
 }
-
-var newPizza;
-var toppingsChosen = [];
-
 
 //---------logic split -----------//
 
 $(document).ready(function() {
   $("form#orderPizza").submit(function(event) {
     event.preventDefault();
-
-    var sizeOrdered = $('#pizzaSize').find('option:selected').text();
-    var numberOfToppings = $("[type='checkbox']:checked").length;
     var toppingsArray = [];
-    var toppingsPush = $("input:checkbox[name=topping]:checked").each(function(){
+
+    var size = $('#pizzaSize').find('option:selected').val();
+
+    $("input:checkbox[name=topping]:checked").each(function(){
       toppingsArray.push($(this).val());
     });
-    newPizza = new Pizza(sizeOrdered, numberOfToppings, toppingsArray);
-    newPizza.pizzaCost(sizeOrdered, numberOfToppings)
-    $("#orderDetails").text("Your order is in!  A " + newPizza.size + "-sized Pizza topped with: " + newPizza.toppingList + " will cost: $" + newPizza.pizzaCost);
+
+    var newPizza = new Pizza(size, toppingsArray);
+
+    newPizza.pizzaCost();
+    $("#orderDetails").text("Your order is in!  A " + newPizza.size + "-sized Pizza topped with: " + newPizza.toppingList + " will cost: $" + newPizza.price);
   });
 });
